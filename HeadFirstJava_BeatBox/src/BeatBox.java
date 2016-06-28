d8b3c9b2804219dcedb3703c9b2bb8e213b61385
hyeongu kang <khgkjg12@naver.com>
2016-06-28 18:22:32 +0900

    initiate

---
+++ HeadFirstJava_BeatBox/src/BeatBox.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.sound.midi.*;
+import java.util.*;
+import java.awt.event.*;
+
+public class BeatBox {
+	JPanel mainpanel;
+	ArrayList<JCheckBox> checkboxList;
+	Sequencer sequencer;
+	Sequence sequence;
+	Track track;
+	JFrame theFrame;
+	
+	String[] instrumentNames = {"Bass Drum","Closed Hi-Hat","Open Hi-Hat","Acoustic Snare","Crash Cymbal","Hand Clap","Hi Tom","Hi Bongo","Marakas","Whistle","Low Conga","Cowbell","Vibraslap","Low-mid Tom","High Agogo","Open Hi Conga"};
+	int[] instruments = {35,42,46,38,49,39,50,60,70,72,64,56,58,47,67,63};
+	
+	public static void main(){
+		new BeatBox.buildGUI();
+	}
+	
+	public void buildGUI(){
+		
+	}
+}
